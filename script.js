13de143f8387d8fda2189005c028441cbe00681f
ratneshmishra2282 <63775997+ratneshmishra2282@users.noreply.github.com>
2026-02-06 18:07:06 +0530

    Update and rename script.js to index.html

--- script.js
+++
@@ -1,9 +0,0 @@
-// Horizontal scroll illusion for About & Services pages
-document.querySelectorAll('.horizontal').forEach(section => {
-  section.addEventListener('wheel', e => {
-    if (window.innerWidth > 768) {
-      e.preventDefault();
-      section.scrollLeft += e.deltaY;
-    }
-  }, { passive: false });
-});
